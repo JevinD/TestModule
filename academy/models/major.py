efd1f72d9c36333cfcd0c933103e215ec4b5a303
Jevin Dement <jdement@opensourceintegrators.com>
2020-08-07 11:49:44 -0700

    formatted XML and added onetomany relationship between session and course

--- academy/models/major.py
+++ academy/models/major.py
@@ -10,11 +10,3 @@ class academyMajor(models.Model):
     degreeType = fields.Selection(
         [("b/s", "B/S"), ("a/a", "A/A")], required=(True), string="Degree Type",
     )
-
-
-class academyCourse(models.Model):
-    _name = "academy.course"
-    _inherit = ["mail.thread", "mail.activity.mixin"]
-    _description = "course "
-
-    course = fields.Char(required=True, string="Course")
--- academy/models/session.py
+++ academy/models/session.py
@@ -9,3 +9,23 @@ class Session(models.Model):
     start_date = fields.Date()
     duration = fields.Float(digits=(6, 2), help="Duration in days")
     seats = fields.Integer(string="Number of seats")
+
+    instructor_id = fields.Many2one("res.partner", string="Instructor")
+    course_id = fields.Many2one(
+        "academy.course", ondelete="cascade", string="Course", required=True
+    )
+    attendee_ids = fields.Many2many("res.partner", string="Attendees")
+
+
+class academyCourse(models.Model):
+    _name = "academy.course"
+    _inherit = ["mail.thread", "mail.activity.mixin"]
+    _description = "Academy courses"
+
+    name = fields.Char(string="Course")
+    description = fields.Text()
+
+    responsible_id = fields.Many2one(
+        "res.users", ondelete="set null", string="Responsible", index=True
+    )
+    session_ids = fields.One2many("academy.session", "course_id", string="Sessions")
