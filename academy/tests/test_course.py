526f74ff610086445691e3484018228a08961256
JevinD <jdement16@gmail.com>
2020-08-17 15:59:58 -0700

    added a working test

--- academy/tests/test_course.py
+++ academy/tests/test_course.py
@@ -3,12 +3,12 @@ from odoo.tests.common import TransactionCase
 
 class TestModelCourse(TransactionCase):
     def test_course_creation(self):
-        responsible_id = self.env["res.users"].search([("login", "=", "beverly")])
+        responsible_id = self.env["res.partner"].search(
+            [("name", "=", "Mitchell Admin")]
+        )
+        # couldnt access the value 3 to run test with responsible_id variable
+        print("\n " + str(responsible_id) + " \n")
         record = self.env["academy.course"].create(
-            {
-                "name": "cs999",
-                "description": "last class",
-                "responsible_id": responsible_id,
-            }
+            {"name": "cs999", "description": "last class", "responsible_id": "3",}
         )
         self.assertEqual(record.name, "cs999")
