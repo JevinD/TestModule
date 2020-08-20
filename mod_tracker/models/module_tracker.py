352177c2d9382a2f41d24a87e531813d3b3f793a
JevinD <jdement16@gmail.com>
2020-08-20 15:25:16 -0700

    changes to mod tracker

--- mod_tracker/models/module_tracker.py
+++ mod_tracker/models/module_tracker.py
@@ -13,21 +13,27 @@ class moduleTracker(models.Model):
     )
     repo_url = fields.Char(string="Github Repo Url")
     rel_date = fields.Date(string="Release Date")
-    customer = fields.Many2many("res.partner", ondelete="set null", string="Customer",)
-    # used for attaching projects to a module
-    # how do I auto fill this or display a selection
-    project_ids = fields.One2many(
-        "project.project", "subtask_project_id", ondelete="cascade", string="Project"
+    # set a one2many within res.partner and create a tab
+    customer = fields.Many2one("res.partner", ondelete="set null", string="Customer",)
+    # CHANGE WHEN SET NEW DATABASE REMOVE THE S@@@@@@@@@@@@@@@@@@@
+    project_ids = fields.Many2one(
+        "project.project", ondelete="cascade", string="Project"
     )
-    # version_sup = fields.many2many()
+    # possible create a configuration menu with the set numbers
+    # for now just set as a char field
+    version_sup = fields.Char(string="Supported Versions")
     prim_designer = fields.Many2one(
         "hr.employee", ondelete="cascade", string="Primary Designer",
     )
+    # SET TO ONE2MANY AND CREATE A MANY2ONE IN THE HR FORM view
     contributor = fields.Many2many("hr.employee", string="Contributors & Developers")
-    dependencies = fields.Many2many("ir.module.module")
-    # special_circum = fields.?
+    dependencies = fields.Char(string="Dependencies")
+    special_circum = fields.Char(string="Special Circumstances")
     # config = fields.?
 
     # example accounting
-    # prim_category = fields.Many2one("ir.module.module")
+    # will be set to the model created in configurations
+    # will need to be set in data file
+    # CHANGE THIS MODEL REF
+    prim_category = fields.Many2one("hr.employee")
     # add_category = fields.One2many
